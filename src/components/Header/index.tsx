9a8e0981ea3553ed800b2d609ea19e53c8f32a70
Volodymyr Oliinyk <volodymyr.oliinyk@flexum.io>
2025-12-20 21:25:43 +0200

    removed useless text and formatted geojson

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,10 +6,6 @@ export default function Header() {
       <div className="header-content">
         <h1 className="header-title">Візуалізація Пожеж</h1>
         <p className="header-subtitle">
-          Якийсь текст
-        
-        </p>
-        <p className="header-subtitle">
       
           Інтерактивна карта з динамікою змін полігонів та інформацією про пожежі
         </p>
